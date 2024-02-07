b16a6c0b82e9b31b77f1e0b0f6ef37536f5445aa
shailesh wagh <shaileshwagh300@gmail.com>
2024-02-07 19:23:43 +0530

    final file added

--- src/Components/Content/Authentication.js
+++ src/Components/Content/Authentication.js
@@ -1,69 +1,114 @@
-import {useState} from 'react'
+import { useState,} from 'react';
 
-const Authentication = () => {
-    const [flag,setflag]=useState(false);
-    let handlesignup=(()=>{
-        setflag(true);
-    })
-    let handleLogin=(()=>{
-        setflag(false);
-    })
+const Authentication = ({setName,setJob}) => {
+  const [flag, setflag] = useState(false);
+  const [formData, setFormData] = useState({
+    name: '',
+    job: '',
+    password: ''
+  });
+
+  let handlesignup = () => {
+    setflag(true);
+  };
+  let handleLogin = () => {
+    setflag(false);
+  };
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    const url = flag ? 'http://localhost:8081/signup' : 'http://localhost:8081/login';
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+      
+      if(response.ok) {
+        const data = await response.json();
+        console.log(data);
+        const token = data.token;
+        if (token) {
+            localStorage.setItem('data', JSON.stringify({ name: formData.name, job: data.job }));
+            localStorage.setItem('flag', true);
+            // setName(data.name);
+            // setJob(data.job);
+            console.log(data);
+          }
+      }
+      else {
+        console.error('Error:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   return (
     <div>
-        <div id='signuplogin'>
-            <button onClick={handlesignup}>SignUp</button>
-            <button onClick={handleLogin}>LogIn</button>
-        </div>
-        <div>
-            {flag
-            ?
-            <div id='signUp'>
-                <form>
-                    <div className='signuplogincss'>
-                        <label className='labelcss'>
-                            Name:
-                        
-                            <input type="text" name="name" />
-                        </label>
-                        <br/>
-                        <label className='labelcss'>
-                            Job:
-                        
-                            <input type="text" name="Job" />
-                        </label>
-                        <br/>
-                        <label className='labelcss'>
-                            password:
-                            <input type="text" name="password" />
-                        </label>
-                        <br/>
-                    </div>
-                    <div class="submitbtn"><input type="submit" class="submitbtn2" value="Submit" /></div>
-                </form>
-            </div>
-            :
-            <div id='login'>
-                <form>
-                    <div className='signuplogincss'>
-                        <label className='labelcss'>
-                            Name:
-                            <input type="text" name="name" />
-                        </label>
-                        <br/>
-                        <label className='labelcss'>
-                            password:
-                            <input type="text" name="password" />
-                        </label>
-                        <br/>
-                    </div>
-                    <div class="submitbtn"><input type="submit" class="submitbtn2" value="Submit" /></div>
-                </form>
-            </div>
-            }
-        </div>
+      <div id="signuplogin">
+        <button onClick={handlesignup}>SignUp</button>
+        <button onClick={handleLogin}>LogIn</button>
+      </div>
+      <div>
+        {flag ? (
+          <div id="signUp">
+            <form onSubmit={handleSubmit}>
+              <div className="signuplogincss">
+                <label className="labelcss">
+                  Name:
+                  <input type="text" name="name" onChange={handleChange} />
+                </label>
+                <br />
+                <label className="labelcss">
+                  Job:
+                  <input type="text" name="job" onChange={handleChange} />
+                </label>
+                <br />
+                <label className="labelcss">
+                  password:
+                  <input type="text" name="password" onChange={handleChange} />
+                </label>
+                <br />
+              </div>
+              <div class="submitbtn">
+                <input type="submit" class="submitbtn2" value="Submit" />
+              </div>
+            </form>
+          </div>
+        ) : (
+          <div id="login">
+            <form onSubmit={handleSubmit}>
+              <div className="signuplogincss">
+                <label className="labelcss">
+                  Name:
+                  <input type="text" name="name" onChange={handleChange} />
+                </label>
+                <br />
+                <label className="labelcss">
+                  password:
+                  <input type="text" name="password" onChange={handleChange} />
+                </label>
+                <br />
+              </div>
+              <div class="submitbtn">
+                <input type="submit" class="submitbtn2" value="Submit" />
+              </div>
+            </form>
+            
+          </div>
+        )}
+      </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Authentication
+export default Authentication;
--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,32 +1,62 @@
-import React from 'react'
-import AppleImg from "../Images/AppleImg.png"
-import GitHubImg from "../Images/GitHubImg.png"
-import PlayStoreImg from "../Images/PlayStoreImg.jpg"
-import ReactImg from "../Images/ReactImg.jpg"
-import Authentication from './Authentication'
-
+import React, { useEffect, useState } from 'react';
+import AppleImg from '../Images/AppleImg.png';
+import GitHubImg from '../Images/GitHubImg.png';
+import PlayStoreImg from '../Images/PlayStoreImg.jpg';
+import ReactImg from '../Images/ReactImg.jpg';
+import Authentication from './Authentication';
 
 const Content = () => {
+  const [name, setName] = useState('______');
+  const [job, setJob] = useState('______');
+  const [flag, setFlag] = useState(localStorage.getItem('flag') === 'true');
+
+  const handleLogout = () => {
+    localStorage.removeItem('data');
+    localStorage.setItem('flag', false);
+    setFlag(false);
+  };
+
+  useEffect(() => {
+    if(flag) {
+      const data = JSON.parse(localStorage.getItem('data'));
+      // if (data) {
+        setName(data.name);
+        setJob(data.job);
+        console.log(data);
+      // }
+    }
+    // else {
+      
+    //   setName('__');
+    //   setJob('__');
+    // }
+  }, [flag]); 
+
   return (
-    <div style={{margin: "30px"}}>
-        <div id="containerOne">
-            <p>My Name is, <span id="name">_______</span></p>
-            <p>i specialize in as <span id="job">_______</span> as following shown,</p>
-            and this is my test submission
-        </div>
+    <div style={{ margin: '30px' }}>
+      <div id="containerOne">
+        <p>
+          My Name is, <span id="name">{name}</span>
+        </p>
+        <p>
+          I specialize in as <span id="job">{job}</span> as following shown,
+        </p>
+        and this is my test submission
+      </div>
 
-        <div id="containerTwo">
-            <img src={ReactImg} className='iconimg' alt='react' style={{border:"2px solid green", borderRadius:"10px"}}></img>
-            <img src={GitHubImg} className='iconimg' alt='GitHubImg' style={{border:"2px solid pink", borderRadius:"10px"}}></img>
-            <img src={AppleImg} className='iconimg' alt='AppleImg' style={{border:"2px solid red", borderRadius:"10px"}}></img>
-            <img src={PlayStoreImg} className='iconimg' alt='PlayStoreImg' style={{border:"2px solid blue", borderRadius:"10px"}}></img>
-        </div>
+      <div id="containerTwo">
+        <img src={ReactImg} className='iconimg' alt='react' style={{ border: '2px solid green', borderRadius: '10px' }} />
+        <img src={GitHubImg} className='iconimg' alt='GitHubImg' style={{ border: '2px solid pink', borderRadius: '10px' }} />
+        <img src={AppleImg} className='iconimg' alt='AppleImg' style={{ border: '2px solid red', borderRadius: '10px' }} />
+        <img src={PlayStoreImg} className='iconimg' alt='PlayStoreImg' style={{ border: '2px solid blue', borderRadius: '10px' }} />
+      </div>
 
-        <div>
-            <Authentication/>
-        </div>
+      <div>
+        <Authentication setName={setName} setjob={setJob}/>
+        {flag && <button onClick={handleLogout}>Logout</button>}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
